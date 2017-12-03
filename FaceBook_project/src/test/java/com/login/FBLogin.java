c6197e9d6682cbbe38e0ea694c2835cc3868dc3e
12 <Lakshmi@DESKTOP-46TS987>
2017-12-03 10:12:27 -0600

    new project

---
+++ FaceBook_project/src/test/java/com/login/FBLogin.java
@@ -0,0 +1,15 @@
+package com.login;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FBLogin {
+
+	public static void main(String[] args) {
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://mvnrepository.com/");
+
+	}
+
+}
